688c241f8d0d8dae2b9a712df1551a11042be043
Matheus Lorenzeto <matheus.mlorenzeto@gmail.com>
2020-06-16 22:26:10 -0300

    add migrations and the functionalities to the modules Customers, Products and Orders

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,63 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer is not registered!');
+    }
+
+    const foundProducts = await this.productsRepository.findAllById(products);
+
+    const updatedProducts = [] as IProduct[];
+
+    const orderedProducts = products.map(orderedProduct => {
+      const product = foundProducts.find(
+        foundProduct => foundProduct.id === orderedProduct.id,
+      );
+
+      if (!product) {
+        throw new AppError('One or more products are not registered!');
+      }
+
+      if (orderedProduct.quantity > product.quantity) {
+        throw new AppError(
+          'Required quantity for one or more products are not disponible.',
+        );
+      }
+
+      product.quantity -= orderedProduct.quantity;
+
+      updatedProducts.push(product);
+
+      const orderedProductWithPrice = {
+        product_id: orderedProduct.id,
+        quantity: orderedProduct.quantity,
+        price: product.price,
+      };
+
+      return orderedProductWithPrice;
+    });
+
+    await this.productsRepository.updateQuantity(updatedProducts);
+
+    const order = this.ordersRepository.create({
+      customer,
+      products: orderedProducts,
+    });
+
+    return order;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1592355654995-CreateOrdersProducts.ts
@@ -0,0 +1,90 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateOrdersProducts1592355654995
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrderProductToOrder',
+        columnNames: ['order_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'orders',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrderProductToProduct',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrderProductToProduct',
+    );
+    await queryRunner.dropForeignKey('orders_products', 'OrderProductToOrder');
+
+    await queryRunner.dropTable('orders_products');
+  }
+}
